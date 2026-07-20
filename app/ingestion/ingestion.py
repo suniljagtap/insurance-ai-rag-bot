31e56b6879f6b740e572085f1854c6fa8c20a41b
Sandipan Giri <girisandipan91@gmail.com>
2026-07-20 11:59:12 +0000

    removed unnecessary comments

--- app/ingestion/ingestion.py
+++ app/ingestion/ingestion.py
@@ -1,13 +1,3 @@
-# Load the pdf file from data folder
-# extract the content
-# arrive at the chunking strategy
-
-# Load the embedding model
-# embed the chunks
-# connect to postges and activate pgvector extension
-# save the vector embeddings and original text in db
-
-# uv add python-dotenv langchain-community pypdf
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -21,9 +11,8 @@ load_dotenv()
 # constants
 chunk_size: int = 800
 chunk_overlap: int = 160
-separators: list = ["\n\n", "\n", ". ", " "]  # put '##' at 2nd position
-
-# ["##", "\n\n", "\n", ". ", " "]  # put '##' at 2nd position
+separators: list = ["\n\n", "\n", ". ", " "]
+file_path = "data/Capstone_Project_4_Insurance_Claims_FAQ.pdf"
 
 
 def load_document(filepath: str):
@@ -57,20 +46,15 @@ def document_splitter(filepath: str):
     splitter = RecursiveCharacterTextSplitter(
         separators=separators, chunk_size=chunk_size, chunk_overlap=chunk_overlap
     )
-    # quote = "Words are flowing out like endless rain into a paper cup,\nthey slither while they pass,\nthey slip away across the universe."
+
     chunks = splitter.split_documents(docs)
-    # chunks = splitter.split_text(quote)
-    # for i, doc in enumerate(chunks):
-    #     print(len(doc.page_content), doc.page_content.replace("\n", " "))
-    #     print("\n")
-    # print([len(chunk["page_content"]) for chunk in chunks])
     return chunks
 
 
 def ingest_pdf(filepath: str):
     """ingesting the document chunks in the vector db"""
-    # 4 load the embedding model & 5 generate the embeddings
-    # 6. save it in vector db
+    # load the embedding model & generate the embeddings
+    # save it in vector db
     chunks = document_splitter(filepath)
     print("ingestion started")
     vector_store = get_vector_store(
@@ -80,58 +64,5 @@ def ingest_pdf(filepath: str):
     print("Ingestion Completed")
 
 
-# def ingest_pdf_old(file_path: str):
-#     """
-#     function to ingest the pdf
-#     """
-#     print("Ingestion Started")
-#     # 1 load pdf
-#     loader = PyPDFLoader(file_path)
-#     docs = loader.load()
-
-#     # 2. Metadata enrichment (for citataion)
-#     for i, doc in enumerate(docs):
-#         doc.metadata.update(
-#             {
-#                 "source": file_path,
-#                 "document_extension": "pdf",
-#                 "page": doc.metadata.get("page"),
-#                 "source_date": doc.metadata.get("creationdate", datetime.today),
-#                 "last_updated": os.path.getmtime(file_path),
-#                 "chunk_index": i,
-#             }
-#         )
-
-#     # print(docs)
-#     # print("Before Chunking")
-#     ### testing
-#     for doc in docs:
-#         print(doc.metadata)
-#         print("\n")
-
-#     # 3. Chunking
-#     splitter = RecursiveCharacterTextSplitter(
-#         chunk_size=800,  # upto 800 characters
-#         chunk_overlap=160,  # upto 160 characters
-#         separators=["\n\n", "\n", ". ", " "],
-#     )
-
-#     chunks = splitter.split_documents(docs)
-#     print("Total Chunks")
-#     print(len(chunks))
-# max_length = max(len(chunk) for chunk in chunks)
-# print("max length of chunk: ", max_length)
-
-# 4 load the embedding model & 5 generate the embeddings
-# 6. save it in vector db
-# vector_store = get_vector_store(collection_name="insurance_claim")
-# vector_store.add_documents(chunks)
-
-# print("Ingestion Completed")
-
-
-# ingest_pdf("data/Capstone_Project_4_Insurance_Claims_FAQ.pdf")
-ingest_pdf("data/Capstone_Project_4_Insurance_Claims_FAQ.pdf")
-# to run this try the following command (from the project root):
-# uv run python -m app.ingestion.ingestion
+ingest_pdf(file_path)
 # uv run python -m ingestion.ingestion
--- app/ingestion/retrieval.py
+++ app/ingestion/retrieval.py
@@ -1,64 +1,30 @@
-# This example demonstrates hybrid retrival (vector and bm25/fts based search )
-# This is good when you are looking for exact keyword based search
-# So, we should build a hybrid retrieval that combines both vector search and keyword search (FTS)
-# because we use pgvector
-# RRF (needed to combine results and rerank them)
-import re
 import psycopg
 import os
 from core.db import get_vector_store
 from langchain_community.retrievers import BM25Retriever
 from langchain_openai import ChatOpenAI
 from collections import defaultdict
-from .ingestion import document_splitter
 
-# from langchain.retrievers
-from psycopg.rows import dict_row
+# from collections import defaultdict
+from .ingestion import document_splitter, file_path
 
-# PGVector connection string uses SQLAlchemy format: postgresql+psycopg://...
-# psycopg.connect needs standard format: postgresql://...
 _raw_conn = os.getenv("PG_CONNECTION_STRING_FTS")
 
+
 # hybrid searching (50/50) searching
-
-
 # Perform cosine similarity vector search (Top-K = 5)
 def vector_search(query, k=5, collection_name: str = "insurance_claim"):
-    print("trying vector search, first method")
+    print("Trying vector search")
     print(query, collection_name)
     vector_store = get_vector_store(collection_name, pre_delete_collection=False)
     results = vector_store.similarity_search_with_score(query=query, k=k)
 
-    ### testing
     document = [
         {"text": doc.page_content, "score": score, "metadata": doc.metadata}
         for doc, score in results
     ]
 
     return document
-
-
-# convert consine distance into similarity score
-def cosine_similarity_search(query, k=5, collection_name: str = "insurance_claim"):
-    print("trying vector search")
-    print(query, collection_name)
-    vector_store = get_vector_store(collection_name, pre_delete_collection=False)
-    results = vector_store.similarity_search_with_score(query=query, k=k)
-    formatted_results = []
-
-    for doc, distance in results:
-
-        similarity = 1 - distance
-
-        formatted_results.append(
-            {
-                "content": doc.page_content,
-                "similarity": similarity,
-                "metadata": doc.metadata,
-            }
-        )
-    print("got documents")
-    return formatted_results
 
 
 def hybrid_search(
@@ -68,7 +34,7 @@ def hybrid_search(
     k=5,
     collection_name: str = "insurance_claim",
 ):
-    chunks = document_splitter("data/Capstone_Project_4_Insurance_Claims_FAQ.pdf")
+    chunks = document_splitter(file_path)
     bm25_retriever = BM25Retriever.from_documents(chunks)
     # Retrieve top 5 keyword matches
     bm25_retriever.k = 5
@@ -111,131 +77,3 @@ def hybrid_search(
     # Sort highest score
     final_results.sort(key=lambda x: x["score"], reverse=True)
     return final_results[:k]
-
-
-if __name__ == "__main__":
-
-    # user_query = {
-    #     "question": "Is this motor insurance claim eligible for coverage?",
-    #     "claim_details": {
-    #         "claim_id": "CLM001",
-    #         "policy_id": "POL00234",
-    #         "claim_type": "Motor",
-    #         "incident_type": "Repair",
-    #         "incident_date": "2026-02-10",
-    #         "reported_delay_days": 2,
-    #         "estimated_damage": 300000,
-    #         "idv": 800000,
-    #         "deductible": 10000,
-    #         "previous_claims_90_days": 2,
-    #         "documents_submitted": ["Policy Copy", "Repair Estimate"],
-    #         "policy_status": "Active",
-    #     },
-    # }
-
-    ### first method trying start
-    # claim = user_query["claim_details"]
-
-    # rag_query = f"""
-    # Question:
-    # {user_query["question"]}
-
-    # Claim Details:
-    # Claim Type: {claim["claim_type"]}
-    # Incident Type: {claim["incident_type"]}
-    # Incident Date: {claim["incident_date"]}
-    # Reported Delay Days: {claim["reported_delay_days"]}
-    # Estimated Damage: {claim["estimated_damage"]}
-    # IDV: {claim["idv"]}
-    # Deductible: {claim["deductible"]}
-    # Previous Claims in 90 Days: {claim["previous_claims_90_days"]}
-    # Policy Status: {claim["policy_status"]}
-    # Documents Submitted: {", ".join(claim["documents_submitted"])}
-    # """
-    # results = vector_store.similarity_search_with_score(query=rag_query, k=5)
-
-    ### first method trying end
-    ### 2nd method
-    # retrieval_query = user_query["question"]
-
-    # vector_store = get_vector_store(
-    #     collection_name="insurance_claim", pre_delete_collection=False
-    # )
-    # documents = vector_store.similarity_search(retrieval_query, k=5)
-
-    # prompt = f"""
-    # You are an insurance claim evaluator.
-
-    # Use the policy documents below to decide eligibility.
-
-    # Policy Documents:
-    # {documents}
-
-    # Claim Details:
-    # {user_query["claim_details"]}
-
-    # Question:
-    # {user_query["question"]}
-
-    # Provide:
-    # 1. Eligibility decision
-    # 2. Reasoning
-    # 3. Relevant policy clauses
-    # """
-    ###2nd method end
-    user_query = " Is a claim automatically rejected if fraud is suspected?"
-    results = cosine_similarity_search(user_query)
-    # print(type(results))
-    # vector_search function
-    # for doc in results:
-    #     print(doc["text"].replace("\n", " "))
-    #     print(doc["score"])
-    # for cosine_similarity function
-    # for doc in results:
-    #     # print(doc)
-    #     print(doc["content"].replace("\n", " "))
-    #     print(doc["similarity"])
-    # uv run python -m app.retrieval.retrieval_v2
-    # uv run python -m ingestion.retrieval
-
-    model = ChatOpenAI(model="gpt-5-mini")
-    context = " ".join([item["content"] for item in results])
-    prompt = f"""
-    Answer the question using only the context.
-    Context:
-    {context}
-    Question:
-    {user_query}
-
-    """
-    response = model.invoke(prompt)
-    print(response.content)
-
-    # query = "What is the IRDAI-mandated maximum turnaround time for settling a health insurance claim? "
-
-    # results = hybrid_search(query)
-    # # for doc in results:
-    # #     # print(doc)
-    # #     print(doc["document"].page_content.replace("\n", " "))
-    # #     print(doc["score"])
-    # #     print(doc["vector_score"])
-    # #     print(doc["bm25_score"])
-
-    # # print(results)
-
-    # # use retrieved doc for RAG
-    # context = "\n\n".join(
-    #     [r["document"].page_content.replace("\n", " ") for r in results]
-    # )
-
-    # from langchain_openai import ChatOpenAI
-
-    # llm = ChatOpenAI(model="gpt-5-mini")
-    # answer = llm.invoke(f"""
-    # Answer using only this context:
-    # {context}
-    # Question:
-    # {query}
-    # """)
-    # print(f"query is {query}")
-    # print(answer.content)
